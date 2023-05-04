4d56cbd5563107c010660bcac73248a5b32cf14a
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-05-04 12:13:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/IPL 2021 - Match Day 2/ipl-2021-match-day-2.py
@@ -0,0 +1,74 @@
+#User function Template for python3
+from collections import deque
+class Solution:
+    def max_of_subarrays(self,arr,n,k):
+        '''
+        you can use collections module here.
+        :param a: given array
+        :param n: size of array
+        :param k: value of k
+        :return: A list of required values 
+        '''
+        #code here
+        q=[]
+        ans=[]
+        start=0
+        end=0
+        
+        while end<n:
+            while q and q[-1]<arr[end]:
+                q.pop()
+            q.append(arr[end])
+            
+            if end-start+1<k:
+                end+=1
+            
+            elif end-start+1==k:
+                ans.append(q[0])
+            
+                if q[0]==arr[start]:
+                    q.pop(0)
+            
+                start+=1
+                end+=1
+        return ans  
+        
+            
+            
+            
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+from collections import deque
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,k = map(int,input().strip().split())
+        arr = list(map(int,input().strip().split()))
+        ob=Solution()
+        res = ob.max_of_subarrays(arr,n,k)
+        for i in range (len (res)):
+            print (res[i], end = " ")
+        print()
+# } Driver Code Ends

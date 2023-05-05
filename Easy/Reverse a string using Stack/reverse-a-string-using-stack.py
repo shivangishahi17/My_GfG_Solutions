1e92f0de98132743ca15b8eceb13e244458789ee
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-05-05 16:49:03 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse a string using Stack/reverse-a-string-using-stack.py
@@ -0,0 +1,24 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
+def reverse(S):
+    stack1=[]
+    stack2=[]
+    for i in S:
+        stack1.append(i)
+        
+    for i in range(len(S)):
+        stack2.append(stack1.pop())
+    return "".join(stack2)
+    #Add code here
+      
+
+#{ 
+ # Driver Code Starts.
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        str1=input()
+        print(reverse(str1))
+# } Driver Code Ends

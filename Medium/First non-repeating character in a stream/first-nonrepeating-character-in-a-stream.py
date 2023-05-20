24a138c6178eb91bd79cf52a0e1af61df1a4fcd2
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-05-20 18:19:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First non-repeating character in a stream/first-nonrepeating-character-in-a-stream.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+from collections import OrderedDict
+class Solution:
+	def FirstNonRepeating(self, A):
+		# Code here
+		s=''
+		d=OrderedDict()
+		for i in A:
+		    if i not in d:
+		        d[i]=1
+		    else:
+		        d[i]+=1
+		    b=False
+    		for k,v in d.items():
+    		    if v==1:
+    		        s+=k
+    		        b=True
+    		        break
+    	    if b==False:
+    		    s+="#"
+	    return s
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		A = input()
+		ob = Solution()
+		ans = ob.FirstNonRepeating(A)
+		print(ans)
+
+# } Driver Code Ends

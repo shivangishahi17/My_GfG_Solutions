84a656a8588e6599e03fc1779322c9efc5259c0d
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-06-02 08:38:53 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Coin Change/coin-change.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def count(self, coins, N, Sum):
+        # code here 
+        dp=[[0 for _ in range(sum+1)] for x in range(N+1)]
+        
+        for i in range(sum+1):
+            dp[0][i]=0
+        
+        for i in range(N+1):
+            dp[i][0]=1
+            
+        for i in range(1, N+1):
+            for j in range(1, sum+1):
+                if coins[i-1]<=j:
+                    dp[i][j]=dp[i][j-coins[i-1]] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[N][Sum]
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+sys.setrecursionlimit(10**6)
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        sum,N = list(map(int, input().strip().split()))
+        coins = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.count(coins,N,sum))
+
+# } Driver Code Ends

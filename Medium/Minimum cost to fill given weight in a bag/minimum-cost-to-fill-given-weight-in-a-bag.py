4301d69599a2cd791f416e461d74cc293775398f
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-06-23 22:37:00 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum cost to fill given weight in a bag/minimum-cost-to-fill-given-weight-in-a-bag.py
+++ Medium/Minimum cost to fill given weight in a bag/minimum-cost-to-fill-given-weight-in-a-bag.py
@@ -3,22 +3,22 @@ import sys
 class Solution:
     def minimumCost(self, cost, n, W):
         # code here
-        a=[[0 for i in range(W+1)] for j in range(n+1)]
+        dp=[[0 for i in range(W+1)] for j in range(n+1)]
 	
 	    for i in range(n+1):
-	        a[i][0]=0
+	        dp[i][0]=0
 	    for i in range(W+1):
-	        a[0][i]=sys.maxsize
+	        dp[0][i]=sys.maxsize
 	        
 	    for i in range(1,n+1):
 	        for j in range(1,W+1):
 	            if(cost[i-1] != -1 and j>=i):
-	                a[i][j]=min(a[i-1][j],a[i][j-i]+cost[i-1])
+	                dp[i][j]=min(dp[i-1][j],dp[i][j-i]+cost[i-1])
 	            else:
-	                a[i][j]=a[i-1][j]
-	    if(a[n][W]==sys.maxsize):
+	                dp[i][j]=dp[i-1][j]
+	    if(dp[n][W]==sys.maxsize):
 	        return -1
-	    return a[n][W]
+	    return dp[n][W]
 
 
 #{ 

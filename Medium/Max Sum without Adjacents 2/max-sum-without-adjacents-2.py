a367eb022882d22eb9301931e53cd74ce6a61e9f
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-06-24 16:10:41 +0530

    Updated Solution - GfG to GitHub

--- Medium/Max Sum without Adjacents 2/max-sum-without-adjacents-2.py
+++ Medium/Max Sum without Adjacents 2/max-sum-without-adjacents-2.py
@@ -5,15 +5,14 @@ class Solution:
 	
 	def findMaxSum(self,arr, n):
 		# code here
-        dp=[-1]*(n)
+        dp=[0]*(n)
         
         dp[0]=arr[0]
         dp[1]=arr[0]+arr[1]
         dp[2]=max(arr[1]+arr[2], arr[0]+arr[2], arr[0]+arr[1])
         
-        if n>3:
-            for i in range(3, n):
-                dp[i]=max(arr[i-1]+arr[i]+dp[i-3], dp[i-2]+arr[i], dp[i-1])
+        for i in range(3, n):
+            dp[i]=max(arr[i-1]+arr[i]+dp[i-3], dp[i-2]+arr[i], dp[i-1])
         return dp[n-1]
 
 #{ 

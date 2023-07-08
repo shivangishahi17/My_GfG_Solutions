d4bfd638f4c7c66977790930bd08a9803e0253fd
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-07-08 23:23:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Form a palindrome/form-a-palindrome.py
@@ -0,0 +1,60 @@
+#User function Template for python3
+
+class Solution:
+    def lcs(self, n, m, s1, s2):
+        dp=[[0 for i in range(m+1)] for j in range(n+1)]
+        
+        for i in range(n+1):
+            dp[i][0]=0
+        
+        for j in range(m+1):
+            dp[0][j]=0
+            
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j], dp[i][j-1])
+        return dp[n][m]
+        
+    def isPalindrome(self, str, n):
+        i=0
+        j=n-1
+        
+        while i<j:
+            if str[i]!=str[j]:
+                return False
+            i+=1
+            j-=1
+        return True
+        
+    def countMin(self, Str):
+        # code here
+        n=len(Str)
+        
+        if self.isPalindrome(Str, n):
+            return 0
+        
+        Str1=Str[::-1]
+        
+        return n-self.lcs(n, n, Str, Str1)
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        Str = input()
+        
+
+        solObj = Solution()
+
+        print(solObj.countMin(Str))
+# } Driver Code Ends

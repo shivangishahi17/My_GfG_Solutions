19dd119a860d7550c0a54f10a8f60bd0046b9b23
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-07-19 16:12:12 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find minimum and maximum element in an array/find-minimum-and-maximum-element-in-an-array.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+def getMinMax( a, n):
+    min=99999999999999
+    max=0
+    for i in range(n):
+        if a[i]>max:
+            max=a[i]
+    for i in range(n):
+        if a[i]<min:
+            min=a[i]
+    return min,max
+    
+    
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        
+        product = getMinMax(a, n)
+        print(product[0], end=" ")
+        print(product[1])
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# } Driver Code Ends

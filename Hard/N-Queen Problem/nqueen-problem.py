122268daa1db5496fd7800d2a302a0afc63b1c06
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-07-29 17:22:46 +0530

    Updated Solution - GfG to GitHub

--- Hard/N-Queen Problem/nqueen-problem.py
+++ Hard/N-Queen Problem/nqueen-problem.py
@@ -1,61 +1,51 @@
 #User function Template for python3
 
 class Solution:
-    def isSafe(self, row, col, temp, n):
-        # row and col
-        for i in range(col, -1, -1):
-            if temp[row][i]==1:
-                return False
-        x=row-1
-        y=col-1
-        while x>=0 and y>=0:
-            if temp[x][y]==1:
-                return False
-            x-=1
-            y-=1
-            
-        x=row+1
-        y=col-1
-            
-        while x<n and y>=0:
-            if temp[x][y]==1:
-                return False
-            x+=1
-            y-=1
-        return True
-            
-    def helper(self, ans, temp, col, n):
-        if col==n:
-            board=[]
-            for i in range(n):
-                for j in range(n):
-                    if temp[j][i]==1:
-                        board.append(j+1)
-            ans.append(board)
-            return
-        for row in range(n):
-            if self.isSafe(row, col, temp, n):
-                temp[row][col]=1
-                self.helper(ans, temp, col+1, n)
-                temp[row][col]=0
+    def isSafe(self, row, col, board, n):
+        duprow=row
+        dupcol=col
         
+        while row>=0 and col>=0:
+            if board[row][col]==1:
+                return False
+            row-=1
+            col-=1
+        
+        col=dupcol
+        row=duprow
+        while col>=0:
+            if board[row][col]==1:
+                return False
+            col-=1
+        
+        row=duprow
+        col=dupcol
+        while row<n and col>=0:
+            if board[row][col]==1:
+                return False
+            row+=1
+            col-=1
+        return True
+        
+    def helper(self, col, board, ans, n, temp):
+        if col==n:
+            ans.append(temp[:])
+            return 
+        for row in range(n):
+            if self.isSafe(row, col, board, n):
+                board[row][col]=1
+                temp.append(row+1)
+                self.helper(col+1, board, ans, n, temp)
+                temp.pop()
+                board[row][col]=0
+                
     def nQueen(self, n):
         # code here
-        
         ans=[]
-        temp=[]
-        for i in range(n):
-            row=[]
-            for j in range(n):
-                row.append(0)
-            temp.append(row)
-        self.helper(ans, temp, 0, n)
+        board=[[0 for i in range(n)] for j in range(n)]
+        self.helper(0, board, ans, n, [])
         return ans
         
-        
- 
-        
-                
         
 
 

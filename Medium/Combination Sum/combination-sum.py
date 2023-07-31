75359ed864b771e221a26a58191e36b23deaeed4
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-07-31 20:48:35 +0530

    Updated Solution - GfG to GitHub

--- Medium/Combination Sum/combination-sum.py
+++ Medium/Combination Sum/combination-sum.py
@@ -10,36 +10,30 @@ import sys
 # } Driver Code Ends
 #User function Template for python3
 
-#User function Template for python3
-
 class Solution:
-    def helper(self, arr, target, i, ans, temp):
-        # Base case
-        if i==len(arr):
-            if target==0:
-                ans.append(temp[:])
-            return
-            
-        if arr[i]<=target:
-            # taking
-            temp.append(arr[i])
-            self.helper(arr, target-arr[i], i, ans, temp)
-            temp.pop()
-        # not taking
-        self.helper(arr, target, i+1, ans, temp)
     
     #Function to return a list of indexes denoting the required 
     #combinations whose sum is equal to given number.
+    def findCombination(self, ind, arr, target, ds, ans):
+        if ind==len(arr):
+            if target==0:
+                ans.append(ds[:])
+            return
+        
+        # pickup the element
+        if arr[ind]<=target:
+            ds.append(arr[ind])
+            self.findCombination(ind, arr, target-arr[ind], ds, ans)
+            ds.pop()
+        # not pick
+        self.findCombination(ind+1, arr, target, ds, ans)
     def combinationalSum(self,A, B):
-    
-        # code here
         A=list(sorted(set(A)))
+        ds=[]
         ans=[]
-        temp=[]
-        self.helper(A, B, 0, ans, temp)
+        self.findCombination(0, A, B, ds, ans)
         return ans
-        
-        
+        # code here
 
 #{ 
  # Driver Code Starts.

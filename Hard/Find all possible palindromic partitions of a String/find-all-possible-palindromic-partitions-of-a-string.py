56f1a26d86cbb72184dbb8b8b52f1f846f4a6901
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-08-01 15:27:03 +0530

    Updated Solution - GfG to GitHub

--- Hard/Find all possible palindromic partitions of a String/find-all-possible-palindromic-partitions-of-a-string.py
+++ Hard/Find all possible palindromic partitions of a String/find-all-possible-palindromic-partitions-of-a-string.py
@@ -1,33 +1,32 @@
 #User function Template for python3
 
 class Solution:
+    def isPalindrome(self, str, start, end):
+        while start<=end:
+            if str[start]!=str[end]:
+                return False
+            start+=1
+            end-=1
+        return True
+    
+    def func(self, ind, str, path, ans):
+        if ind==len(str):
+            ans.append([ele for ele in path])
+            return 
+            
+        for i in range(ind, len(str)):
+            if self.isPalindrome(str, ind, i):
+                path.append(str[ind:i+1])
+                self.func(i+1, str, path, ans)
+                path.pop()
+                
     def allPalindromicPerms(self, S):
         # code here 
-        def helper(index, s, temp, ans):
-            if index == len(s):
-                ans.append([ele for ele in temp])
-                return
-            for i in range(index, len(s)):
-                if isPalindrome(s, index, i):
-                    temp.append(s[index:i+1])
-                    helper(i+1, s, temp, ans)
-                    temp.pop()
-        def isPalindrome(s, start, end):
-            while start <= end:
-                if s[start] != s[end]:
-                    return False
-                start += 1
-                end -= 1
-            return True
-        
-        ans = []
-        temp = []
-        helper(0, S, temp, ans)
+        ans=[]
+        path=[]
+        self.func(0, S, path, ans)
         return ans
         
-        
-       
-
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3

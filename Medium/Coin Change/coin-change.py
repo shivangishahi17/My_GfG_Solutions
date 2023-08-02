de657feb4e169f875baa6c7148cd3ee5c67b6c7d
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-08-02 14:01:58 +0530

    Updated Solution - GfG to GitHub

--- Medium/Coin Change/coin-change.py
+++ Medium/Coin Change/coin-change.py
@@ -1,25 +1,28 @@
 #User function Template for python3
 
 class Solution:
+    # using Recursion
+    # T.C.>2^N, S.C>>O(N)~=O(Target)
+    
+        
     def count(self, coins, N, Sum):
         # code here 
-        dp=[[0 for _ in range(sum+1)] for x in range(N+1)]
+        dp=[[0 for i in range(sum+1)] for i in range(N+1)]
         
         for i in range(N+1):
             dp[i][0]=1
-            
-        for i in range(sum+1):
+        
+        for i in range(Sum+1):
             dp[0][i]=0
             
         for i in range(1, N+1):
-            for j in range(1, sum+1):
+            for j in range(1, Sum+1):
                 if coins[i-1]<=j:
                     dp[i][j]=dp[i][j-coins[i-1]] + dp[i-1][j]
                 else:
                     dp[i][j] = dp[i-1][j]
         return dp[N][Sum]
-
-
+        
 
 #{ 
  # Driver Code Starts

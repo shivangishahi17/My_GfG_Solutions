87f72dbdabcb44f29fe839d0a82509746089e775
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-09-16 15:35:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Permutations of a given string/permutations-of-a-given-string.py
+++ Medium/Permutations of a given string/permutations-of-a-given-string.py
@@ -1,29 +1,29 @@
 #User function Template for python3
 
 class Solution:
-
+    def permute(self, arr, ans, ind):
+        if ind==len(arr):
+            if "".join(arr) not in ans:
+                ans.append("".join(arr))
+            return
+        
+        for i in range(ind, len(arr)):
+            arr[i], arr[ind]=arr[ind], arr[i]
+            self.permute(arr, ans, ind+1)
+            arr[i], arr[ind]=arr[ind], arr[i]
+                
+        
     def find_permutation(self, S):
         # Code here
-        def solve(arr, ans, idx):
-            if idx==len(arr):
-                # condition to check duplicates
-                if "".join(arr) not in ans:
-                    ans.append("".join(arr))
-            for i in range(idx, len(arr)):
-                arr[idx], arr[i]=arr[i], arr[idx]
-                solve(arr, ans, idx+1)
-                arr[idx], arr[i]=arr[i], arr[idx]
-                
-        ans=[]
+        # T.C.=O(N)*N!=O(N) for loop and N! for generating permutation
+        # S.C.=O(1) apart from auxiliaary space of recursion depth Here O(N) is recursion space and O(N!)
+        # space for returning the answer
+        ind=0
         arr=list(S)
-        solve(arr, ans, 0)
-        ans.sort()
+        ans=[]
+        self.permute(arr, ans, ind)
         return ans
-        
-        
 
-     
-        
 
 
 #{ 
@@ -37,6 +37,7 @@ if __name__ == '__main__':
 		S=input()
 		ob = Solution()
 		ans = ob.find_permutation(S)
+		ans.sort()
 		for i in ans:
 			print(i,end=" ")
 		print()

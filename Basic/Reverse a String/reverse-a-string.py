756178ba5034bca84e57dbbf166c0214313ebc3f
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-11-21 20:33:51 +0530

    Updated Solution - GfG to GitHub

--- Basic/Reverse a String/reverse-a-string.py
+++ Basic/Reverse a String/reverse-a-string.py
@@ -1,15 +1,19 @@
 #User function Template for python3
 
-def reverseWord(s):
-    #your code here
-    s=list(s)
-    i=0
-    j=len(s)-1
-    while i<=j:
-        s[i], s[j]=s[j], s[i]
-        i+=1
-        j-=1
-    return "".join(s)
+class Solution:
+    def reverse(self, s):
+        start=0
+        end=len(s)-1
+        while start<end:
+            s[start], s[end]=s[end], s[start]
+            start+=1
+            end-=1
+        return "".join(s)
+        
+    def reverseWord(self, str: str) -> str:
+        #your code here
+        s=list(str)
+        return self.reverse(s)
 
 
 #{ 
@@ -20,7 +24,8 @@ if __name__ == "__main__":
     t = int(input())
     while(t>0):
         s = input()
-        print(reverseWord(s))
+        ob = Solution()
+        print(ob.reverseWord(s))
         t = t-1
 
 # } Driver Code Ends

d5f63154af7855805cedd702e8b9e5165a3fd81f
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-11-29 19:59:03 +0530

    Updated Solution - GfG to GitHub

--- Medium/Count Inversions/count-inversions.py
+++ Medium/Count Inversions/count-inversions.py
@@ -4,97 +4,48 @@ class Solution:
     # arr[]: Input Array
     # N : Size of the Array arr[]
     #Function to count inversions in the array.
+    def merge(self, arr, low, mid, high):
+        temp=[]
+        left=low
+        right=mid+1
+        
+        cnt=0
+        while left<=mid and right<=high:
+            if arr[left]<=arr[right]:
+                temp.append(arr[left])
+                left+=1
+            else:
+                temp.append(arr[right])
+                cnt+=(mid-left+1)
+                right+=1
+        
+        while left<=mid:
+            temp.append(arr[left])
+            left+=1
+        
+        while right<=high:
+            temp.append(arr[right])
+            right+=1
+        
+        for i in range(low, high+1):
+            arr[i]=temp[i-low]
+        return cnt
+        
+    def mergeSort(self, arr, low, high):
+        cnt=0
+        if low>=high:
+            return cnt
+        mid=(low+high)//2
+        cnt+=self.mergeSort(arr, low, mid)
+        cnt+=self.mergeSort(arr, mid+1, high)
+        cnt+=self.merge(arr, low, mid, high)
+        return cnt
+        
     def inversionCount(self, arr, n):
         # Your Code Here
+        return self.mergeSort(arr, 0, n-1)
         
-        temp_arr = [0]*n
-        return self._mergeSort(arr, temp_arr, 0, n-1)
-     
-    # This Function will use MergeSort to count inversions
-     
-    def _mergeSort(self, arr, temp_arr, left, right):
-     
-        # A variable inv_count is used to store
-        # inversion counts in each recursive call
-     
-        inv_count = 0
-     
-        # We will make a recursive call if and only if
-        # we have more than one elements
-     
-        if left < right:
-     
-            # mid is calculated to divide the array into two subarrays
-            # Floor division is must in case of python
-     
-            mid = (left + right)//2
-     
-            # It will calculate inversion
-            # counts in the left subarray
-     
-            inv_count += self._mergeSort(arr, temp_arr,
-                                    left, mid)
-     
-            # It will calculate inversion
-            # counts in right subarray
-     
-            inv_count += self._mergeSort(arr, temp_arr,
-                                    mid + 1, right)
-     
-            # It will merge two subarrays in
-            # a sorted subarray
-     
-            inv_count += self.merge(arr, temp_arr, left, mid, right)
-        return inv_count
-     
-    # This function will merge two subarrays
-    # in a single sorted subarray
-     
-     
-    def merge(self, arr, temp_arr, left, mid, right):
-        i = left     # Starting index of left subarray
-        j = mid + 1  # Starting index of right subarray
-        k = left     # Starting index of to be sorted subarray
-        inv_count = 0
-     
-        # Conditions are checked to make sure that
-        # i and j don't exceed their
-        # subarray limits.
-     
-        while i <= mid and j <= right:
-     
-            # There will be no inversion if arr[i] <= arr[j]
- 
-            if arr[i] <= arr[j]:
-                temp_arr[k] = arr[i]
-                k += 1
-                i += 1
-            else:
-                # Inversion will occur.
-                temp_arr[k] = arr[j]
-                inv_count += (mid-i + 1)
-                k += 1
-                j += 1
-     
-        # Copy the remaining elements of left
-        # subarray into temporary array
-        while i <= mid:
-            temp_arr[k] = arr[i]
-            k += 1
-            i += 1
-     
-        # Copy the remaining elements of right
-        # subarray into temporary array
-        while j <= right:
-            temp_arr[k] = arr[j]
-            k += 1
-            j += 1
-     
-        # Copy the sorted subarray into Original array
-        for loop_var in range(left, right + 1):
-            arr[loop_var] = temp_arr[loop_var]
-     
-        return inv_count
+
 
 #{ 
  # Driver Code Starts

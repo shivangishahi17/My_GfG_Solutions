2123691a3e272b04f9d9c64599e5bce756386a70
shivangishahi17 <116482348+shivangishahi17@users.noreply.github.com>
2023-12-09 16:30:37 +0530

    Updated Solution - GfG to GitHub

--- Hard/Merge Without Extra Space/merge-without-extra-space.py
+++ Hard/Merge Without Extra Space/merge-without-extra-space.py
@@ -5,18 +5,18 @@ class Solution:
     #Function to merge the arrays.
     def merge(self,arr1,arr2,n,m):
         #code here
-        left=n-1
-        right=0
-        while left>=0 and right<m:
-            if arr1[left]>arr2[right]:
-                arr1[left], arr2[right]=arr2[right], arr1[left]
-                left-=1
-                right+=1
+        left = n-1
+        right = 0
+        
+        while left >= 0 and right < m:
+            if arr1[left] > arr2[right]:
+                arr1[left], arr2[right] = arr2[right], arr1[left]
+                left -= 1
+                right += 1
             else:
                 break
         arr1.sort()
         arr2.sort()
-            
     
 
 
